63da7497dd84b6c140be67b408a7a4792f463d72
KemishBautista <kembaga44@gmail.com>
2022-05-31 16:28:30 -0700

    Version 2

--- EscrituraArchivos/EscrituraArchivos/Program.cs
+++ EscrituraArchivos/EscrituraArchivos/Program.cs
@@ -11,22 +11,24 @@ namespace EscrituraArchivos
     {
         static void Main(string[] args)
         {
-            StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+            StreamWriter sw = new StreamWriter("Ejemplo.txt", true);
             //Si el archivo no existe creara el documento, si ya existe escribira en el 
             //True es para agregar y sobreescribir 
 
-            string[] lineas =
-            { "primera linea ", 
-              "segunda linea", 
-              "tercera linea" 
-            };
+            string[] lineas = new string[2];
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write("Escribe un nombre: ");
+                lineas[i] = Console.ReadLine();
+            }
 
             //Recorre el arreglo 
             foreach (string line in lineas)
             {
                 sw.WriteLine(line);
             }
-            sw.Close();
+            sw.Close();//El archivo siempre se cierra
 
             Console.WriteLine("Escribiendo archivo...");
             Console.ReadKey();
