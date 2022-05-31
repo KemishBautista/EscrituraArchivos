c525c014a079347b0c8ea7236745c30d26a8297f
KemishBautista <kembaga44@gmail.com>
2022-05-31 16:25:31 -0700

    Version 1

---
+++ EscrituraArchivos/EscrituraArchivos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EscrituraArchivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+            //Si el archivo no existe creara el documento, si ya existe escribira en el 
+            //True es para agregar y sobreescribir 
+
+            string[] lineas =
+            { "primera linea ", 
+              "segunda linea", 
+              "tercera linea" 
+            };
+
+            //Recorre el arreglo 
+            foreach (string line in lineas)
+            {
+                sw.WriteLine(line);
+            }
+            sw.Close();
+
+            Console.WriteLine("Escribiendo archivo...");
+            Console.ReadKey();
+        }
+    }
+}
